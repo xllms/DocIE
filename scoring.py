279057167c52bdd50e220d5b66cd83b8422317c9
ZhenbinAI <zhenbin_chan@163.com>
2025-03-20 14:59:03 +0800

    update scoring.py

--- scoring.py
+++ scoring.py
@@ -2,14 +2,15 @@ import json
 import sys, os
 
 
-def traverse_dir(path):
-    for root, dirs, files in os.walk(path):
-        print("当前目录：", root)
-        print("子目录列表：", dirs)
-        print("文件列表：", files)
+
+res_file = "../input/res/results.json" # The path of the prediction file (results.json)
+ref_file = "../input/ref/reference.json" # The path of the Ground True file (reference.json)
+output_dir = "../output" #The path of the output directory
+scores_file = os.path.join(output_dir, "scores.json")#The path of the output file (scores.json)
 
 
-traverse_dir("../")
+
+
 
 # EI counter
 EI_tp = 0
@@ -29,10 +30,7 @@ RE_pre_len = 0
 
 cnt = 0
 
-res_file = "../input/res/results.json"
-ref_file = "../input/ref/reference.json"
-output_dir = "../output"
-scores_file = os.path.join(output_dir, "scores.json")
+
 
 # res_file = "D:/codabench/res.json"
 # ref_file = "D:/codabench/DocIE_curr copy/reference_data/reference.json"
@@ -42,6 +40,12 @@ scores_file = os.path.join(output_dir, "scores.json")
 
 
 #################### Methods ####################
+def traverse_dir(path):
+    for root, dirs, files in os.walk(path):
+        print("Current Directory", root)
+        print("Parents：", dirs)
+        print("File List：", files)
+
 def get_mention_list(head_entity, mention_for_re):
     for mtlist in mention_for_re:
         if head_entity in mtlist:
@@ -74,6 +78,7 @@ def compute_f1(cnt, tp, pred_num, gold_num):
 
 
 ########### Processing Ground True ##############
+traverse_dir("../")
 # Lodading Ground True
 with open(ref_file, "r", encoding="utf-8") as gt_file:
     # import pdb;pdb.set_trace()
@@ -81,17 +86,7 @@ with open(ref_file, "r", encoding="utf-8") as gt_file:
         gt_file
     )  # Ground_True[0]['id', 'domain', 'title', 'doc', 'entities', 'triples', 'label_set', 'entity_label_set']
 
-"""
-将Ground True 处理成以下格式，方便检索，其中key为doc id。
-{0: {'mentions_GT': 
-        [{'William Thomson, 1st Baron Kelvin', 'Baron Kelvin', 'William Thomson', 'Kelvin', '1st Baron Kelvin'}, {'1st'}, {'land line', 'long cable', 'cable', 'Wheatstone transmitter', 'cable signals'}, {'1876'}, {'Eastern Telegraph Company'}, {'telegraph key', 'cable sender', 'automatic curb sender', 'self-acting cable key'}, {'Eastern Telegraph Company'}], 
-    'relations_GT': 
-        [('Wheatstone transmitter', 'LocatedIn', 'land line'), ('automatic curb sender', 'PartOf', 'telegraph key'), ('automatic curb sender', 'UsedBy', 'Eastern Telegraph Company'), ('automatic curb sender', 'UsedBy', 'Eastern Telegraph Company'), ('William Thomson', 'Creator', 'automatic curb sender'), ('automatic curb sender', 'LocatedIn', 'submarine communications cable'), ('cable signals', 'InfluencedBy', 'currents'), ('prolongation of the signals', 'InfluencedBy', 'electromagnetic induction')]}
-1: {...},
-2: {...},
-...
-}
-"""
+
 GT = {}
 for doc_id, sample in Ground_True.items():
 
@@ -151,18 +146,16 @@ for pre_id, sample in results.items():
         for j in range(len(mention_gt_list)):
             if (
                 mention_pred[i]["mentions"] in mention_gt_list
-            ):  # mention 全部抽取正确 EC 正确数量加1
+            ): 
                 EI_tp += 1
                 type_idx = mention_gt_list.index(
                     mention_pred[i]["mentions"]
-                )  # 根据mention求id，id再找type
+                ) 
 
                 if mention_pred[i]["type"] == type_gt_list[type_idx]:
-                    # 如果类型还相等，EC+1
                     EC_tp += 1
                     break
                 else:
-                    # 如果不相等，直接下一个预测样本
                     break
 
     # ================  relations  ================
@@ -175,7 +168,7 @@ for pre_id, sample in results.items():
         pred_triple = (pred["head"], pred["relation"], pred["tail"])
         if (
             pred_triple in relation_gt_list
-        ):  # 判断预测的三元组在不在GT中,如果不在，再判断是否符合宽松模式
+        ):  # Determine whether the predicted triple is in GT. If not, determine whether it meets the general mode.
             RE_GEN_tp += 1
             RE_STRICT_tp += 1
         else:
@@ -194,7 +187,7 @@ for pre_id, sample in results.items():
 
 print("#" * 20, "Entity Identification", "#" * 20)
 entity_identification_res = compute_f1(cnt, EI_tp, EI_pred_len, EI_gold_len)
-print("Correct Mention list:", EI_tp)  # 更改了id:0 的第一个mention list，删除了一个元素
+print("Correct Mention list:", EI_tp)
 print("Predict Mentions:", EI_pred_len)
 print("GT Mentions:", EI_gold_len)
 
@@ -202,7 +195,7 @@ print("#" * 20, "Entity Classification", "#" * 20)
 entity_classification_res = compute_f1(cnt, EC_tp, EC_pred_len, EC_gold_len)
 print(
     "Correct Mention Classfication:", EC_tp
-)  # 更改了 id：1 和 id：2 的type ,比上面多错2个，检查完毕
+)  
 print("Predict Mentions Classfication:", EC_pred_len)
 print("GT Mentions Classfication:", EC_gold_len)
 
@@ -210,7 +203,7 @@ print("#" * 20, "RE General Mode", "#" * 22)
 re_general_res = compute_f1(cnt, RE_GEN_tp, RE_pre_len, RE_gold_len)
 print(
     "Correct Relations:", RE_GEN_tp
-)  # 将第一个relation reiples 的 head entity 换成了它的mention，应该它比STRICT多1, 检查完毕
+)  
 print("Predict Relations:", RE_pre_len)
 print("GT Relations:", RE_gold_len)
 
